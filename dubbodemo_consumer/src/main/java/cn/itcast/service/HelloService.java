3970b89f0a62305270845ed9e389151395fbc5d4
youknow <xuchengfeifei@163.com>
2020-12-19 19:58:53 +0800

    修改了 HelloService 接口02

--- dubbodemo_consumer/src/main/java/cn/itcast/service/HelloService.java
+++ dubbodemo_consumer/src/main/java/cn/itcast/service/HelloService.java
@@ -2,5 +2,6 @@ package cn.itcast.service;
 
 public interface HelloService {
     String sayHello(String name);
-    String sayCodeGod(String name1);
+    String sayCodeGod(String name);
+    String pratice(String haha);
 }
